728bd838c653a7d9bccac47de7997af7ffbfe84d
weiqi <kevew.wei@mail.utoronto.ca>
2025-03-18 19:19:00 -0400

    Progress on milestone 3. Weird bug

--- drmario.asm
+++ drmario.asm
@@ -32,8 +32,11 @@ colors: .word 0x00ff00, 0xff0000, 0x0000ff, 0x000000  # Green, Red, Blue, Black
 
 ##############################################################################
 # Mutable Data
-##############################################################################
 
+# State of the current board
+# Think of it as a 27x15 array.
+# If a index at a array is 
+board: .space 405 # 27 rows x 15 columns = 405
 ##############################################################################
 # Code
 ##############################################################################
@@ -42,7 +45,7 @@ colors: .word 0x00ff00, 0xff0000, 0x0000ff, 0x000000  # Green, Red, Blue, Black
 	
 # Before beginning, here is some information for how I'm storing my data in the registers
 # $s0 = base address for display
-# $s1 = address to the board
+# $s1 = address to the board state
 # $s3 = Pill 1 Position
 # $s4 = Pill 2 Position
 # For example: Say we have the pill like the following
@@ -62,6 +65,22 @@ colors: .word 0x00ff00, 0xff0000, 0x0000ff, 0x000000  # Green, Red, Blue, Black
     # Run the game.
 main:
     lw $s0, ADDR_DSPL
+    la $s1, board # Load in the basic board address to $s1
+    
+    # Instantiate the board as filled with zeros.
+    la $t0, board
+    li $t1, 405       # Total bytes to initialize (27*15 = 405)
+    li $t2, 0         # Value to store (0)
+    set_zero_board:
+        beqz $t1, exit_zero_board     # Exit loop when counter ($t1) reaches 0
+        sb $t2, 0( $t0 )    # Store 0 at current address
+        addi $t0, $t0, 1  # Move to next byte
+        addi $t1, $t1, -1 # Decrement counter
+        j set_zero_board  # Repeat
+    exit_zero_board:
+
+    
+    
     # Initialize the game
     jal instantiate_map # Save return address at $ra and calls instantiate map to build map
     
@@ -99,16 +118,26 @@ random_color:
     
 # This function instializes the map boundaries
 instantiate_map: 
-    li $t1, 0x808080 # $t1 = gray
+    addi $sp, $sp, -4        # Allocate space on the stack
+    sw $ra, 0($sp)           # Save the original return address
+    
+    li $t7, 0x808080 # $t1 = gray
     
     # Draw the left gray wall
     addi $t3, $zero, 1652 # t3 tracks the current location starting from the top
     addi $t4, $zero, 8052 # t4 is the bottem row
     wall_draw_left:
-        beq $t3, $t4 wall_draw_left_end # Checks if reached the bottom yet.
+        beq $t3, $t4, wall_draw_left_end # Checks if reached the bottom yet.
+        # This section draws it on the bitmap
         add $t5, $s0, $t3 # If it has not, update $t5 which is where we want to draw
-        sw $t1, 0( $t5 ) # Draw the pixel
+        sw $t7, 0( $t5 ) # Draw the pixel
         addi $t3, $t3, 256 # Move down a row
+        
+        # This section adds it to the board state
+        addi $a0, $t5, 0
+        addi $a1, $zero, 1
+        jal set_board_by_addr
+        
         j wall_draw_left
     wall_draw_left_end:
     
@@ -116,10 +145,16 @@ instantiate_map:
     addi $t3, $zero, 1716 # t3 tracks the current location starting from the top
     addi $t4, $zero, 8116 # t4 is the bottem row
     wall_draw_right:
-        beq $t3, $t4 wall_draw_right_end # Checks if reached the bottom yet.
+        beq $t3, $t4, wall_draw_right_end # Checks if reached the bottom yet.
         add $t5, $s0, $t3 # If it has not, update $t5 which is where we want to draw
-        sw $t1, 0( $t5 ) # Draw the pixel
+        sw $t7, 0( $t5 ) # Draw the pixel
         addi $t3, $t3, 256 # Move down a row
+        
+        # This section adds it to the board state
+        addi $a0, $t5, 0
+        addi $a1, $zero, 1
+        jal set_board_by_addr
+        
         j wall_draw_right
     wall_draw_right_end:
     
@@ -127,9 +162,9 @@ instantiate_map:
     addi $t3, $zero, 7796 # t3 tracks the current location starting from the left
     addi $t4, $zero, 7864 # t4 is the right part
     wall_bottom:
-        beq $t3, $t4 wall_bottom_end # Checks if reached the bottom yet.
+        beq $t3, $t4, wall_bottom_end # Checks if reached the bottom yet.
         add $t5, $s0, $t3 # If it has not, update $t5 which is where we want to draw
-        sw $t1, 0( $t5 ) # Draw the pixel
+        sw $t7, 0( $t5 ) # Draw the pixel
         addi $t3, $t3, 4 # Move down a row
         j wall_bottom
     wall_bottom_end:
@@ -138,33 +173,35 @@ instantiate_map:
     addi $t3, $zero, 1656 # t3 tracks the current location starting from the left
     addi $t4, $zero, 1680 # t4 is the right part
     wall_top_left: # Draw the left top part
-        beq $t3, $t4 wall_top_left_end # Checks if reached the bottom yet.
+        beq $t3, $t4, wall_top_left_end # Checks if reached the bottom yet.
         add $t5, $s0, $t3 # If it has not, update $t5 which is where we want to draw
-        sw $t1, 0( $t5 ) # Draw the pixel
+        sw $t7, 0( $t5 ) # Draw the pixel
         addi $t3, $t3, 4 # Move down a row
         j wall_top_left
     wall_top_left_end: 
     addi $t3, $zero, 1692 # t3 tracks the current location starting from the left
     addi $t4, $zero, 1716 # t4 is the right part
     wall_top_right: #Draw the right top part
-        beq $t3, $t4 wall_top_right_end # Checks if reached the bottom yet.
+        beq $t3, $t4, wall_top_right_end # Checks if reached the bottom yet.
         add $t5, $s0, $t3 # If it has not, update $t5 which is where we want to draw
-        sw $t1, 0( $t5 ) # Draw the pixel
+        sw $t7, 0( $t5 ) # Draw the pixel
         addi $t3, $t3, 4 # Move down a row
         j wall_top_right
     wall_top_right_end:
     
     # Now manually draw the top part
     addi $t3, $s0, 1420
-    sw $t1, 0( $t3 )
+    sw $t7, 0( $t3 )
     addi $t3, $s0, 1164
-    sw $t1, 0( $t3 )
+    sw $t7, 0( $t3 )
     addi $t3, $s0, 1436
-    sw $t1, 0( $t3 )
+    sw $t7, 0( $t3 )
     addi $t3, $s0, 1180
-    sw $t1, 0( $t3 ) 
+    sw $t7, 0( $t3 ) 
     
-    jr $ra # Returns to the main function
+    lw $ra, 0($sp)           # Restore original return address
+    addi $sp, $sp, 4         # Free stack space
+    jr $ra                   # Return to main function
 
 
 
@@ -230,16 +267,60 @@ addr_to_board:
     
     jr $ra  # Return to caller
 
-# Input: $a0 = X (row), $a1 = Y (col)
-# Output: $v0 = address offset
-board_to_addr:
-    li $t0, 256 # Bytes per row (64 columns * 4 bytes per pixel)
-    mul $t1, $a0, $t0 # $t1 = X * bytes per row
-    sll $t2, $a1, 2 # $t2 = Y * 4 (bytes)
-    add $t3, $t1, $t2 # $t3 = offset within the board
-    addi $v0, $t3, 1140 # Add the board's top-left offset (1912)
-    
+# Get the value at the index board[$a0][$a1].
+# For example board[i][0] where i >= 2 is 5 because it's the wall.
+get_val_at_board:
+    mul $t8, $a0, 15
+    add $t8, $t8, $a1 # Get the index in the board
+    add $t8, $t8, $s1 # Get the address on the board
+    lb $v0, 0 ($t8)
     jr $ra
+
+# The two functions above finally allows us to define the next two functions. The first loads a value $a1 into the board at $a0. The second reads
+#--------------------------------------------------------------
+# Sets the board value corresponding to a bitmap address.
+# Input: $a0 = bitmap address, $a1 = value to set
+#--------------------------------------------------------------
+set_board_by_addr:
+    addi $sp, $sp, -4        # Allocate space on the stack
+    sw $ra, 0($sp)           # Save the original return address
+
+
+    sub $a0, $a0, $s0
+    jal addr_to_board
+    add $t0, $v0, $zero # X position of Pill 1 on board
+    add $t1, $v1, $zero # Y position of Pill 1 on board
+    
+    mul $t8, $t0, 15
+    add $t8, $t8, $t1 # Get the index in the board
+    add $t8, $t8, $s1 # Get the address on the board
+    sb $a1, 0 ($t8)   # Set value at the board
+   
+    lw $ra, 0($sp)           # Restore original return address
+    addi $sp, $sp, 4         # Free stack space
+    jr $ra                   # Return to caller
+    
+    
+#--------------------------------------------------------------
+# Gets the board value corresponding to a bitmap address.
+# Input:  $a0 = bitmap address
+# Output: $v0 = value at the board
+#--------------------------------------------------------------
+get_board_by_addr:
+    addi $sp, $sp, -4        # Allocate space on the stack
+    sw $ra, 0($sp)           # Save the original return address
+
+    sub $a0, $a0, $s0
+    jal addr_to_board
+    add $a0, $v0, $zero # X position of Pill 1 on board
+    add $a1, $v1, $zero # Y position of Pill 1 on board
+    jal get_val_at_board
+    
+    lw $ra, 0($sp)           # Restore original return address
+    addi $sp, $sp, 4         # Free stack space
+    jr $ra                   # Return to caller
+    
+    
     
 # Central function that handles all the keyboard inputs
 keybord_input: 
@@ -266,14 +347,52 @@ respond_to_S:
     
 # Move left the current pill
 respond_to_A:
-    addi $s3, $s3, -4
-    addi $s4, $s4, -4
+    # Check if current pill is horizontal or not
+    addi $t4, $s3, 4
+    bne $s4, $t4, vertical_left_move
+        # If the pill is currently horizontal, this means we only need to check the left of pill 1
+        addi $a0, $s3, -4
+        jal get_board_by_addr
+        bne $v0, 0, finite_no_left_movement 
+        j finite_left_movement
+    # If the pill is currently vertical, this means we need to check the left of both pill 1 and 2.
+    vertical_left_move:
+        addi $a0, $s3, -4
+        jal get_board_by_addr
+        bne $v0, 0, finite_no_left_movement 
+        addi $a0, $s4, -4
+        jal get_board_by_addr
+        bne $v0, 0, finite_no_left_movement 
+        j finite_left_movement
+    finite_left_movement:
+        addi $s3, $s3, -4
+        addi $s4, $s4, -4
+    finite_no_left_movement:
     j update_board
     
-# Move right the current pill
+# Move left the current pill
 respond_to_D:
-    addi $s3, $s3, 4
-    addi $s4, $s4, 4
+    # Check if current pill is horizontal or not
+    addi $t4, $s3, 4
+    bne $s4, $t4, vertical_right_move
+        # If the pill is currently horizontal, this means we only need to check the right of pill 2
+        addi $a0, $s4, 4
+        jal get_board_by_addr
+        bne $v0, 0, finite_no_right_movement 
+        j finite_right_movement
+    # If the pill is currently vertical, this means we need to check the right of both pill 1 and 2.
+    vertical_right_move:
+        addi $a0, $s3, 4
+        jal get_board_by_addr
+        bne $v0, 0, finite_no_right_movement 
+        addi $a0, $s4, 4
+        jal get_board_by_addr
+        bne $v0, 0, finite_no_right_movement 
+        j finite_right_movement
+    finite_right_movement:
+        addi $s3, $s3, 4
+        addi $s4, $s4, 4
+    finite_no_right_movement:
     j update_board
 
 
@@ -296,7 +415,7 @@ respond_to_W:
     
     # Updates the pill
     addi $t2, $t1, 1
-    bne $t2, $t4 rotate_h_v
+    bne $t2, $t4, rotate_h_v
     # If the pill is currently horizontal
     addi $s4, $s3, -256
     j finish_rotate
